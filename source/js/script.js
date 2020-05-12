58c439b1cd4be2c50cdf6d2565bc2c9fae8d4aea
iramilyukova <60115026+iramilyukova@users.noreply.github.com>
2020-05-12 20:07:58 +0300

    Доделки

--- source/js/script.js
+++ source/js/script.js
@@ -1,164 +1,140 @@
-  var navMain = document.querySelector(".main-nav");
-  var navButtonClose = document.querySelector(".main-nav__toggle");
-  var navButtonOpen = document.querySelector(".main-nav__menu");
+var navMain = document.querySelector(".main-nav");
+var navButtonClose = document.querySelector(".main-nav__toggle");
+var navButtonOpen = document.querySelector(".main-nav__menu");
 
-  navMain.classList.remove("main-nav--nojs");
+navMain.classList.remove("main-nav--nojs");
 
-  navButtonClose.addEventListener("click", function() {
-    if (navMain.classList.contains("main-nav--opened")) {
-      navMain.classList.remove("main-nav--opened");
-      navMain.classList.add("main-nav--closed");
+navButtonClose.addEventListener("click", function () {
+  if (navMain.classList.contains("main-nav--opened")) {
+    navMain.classList.remove("main-nav--opened");
+    navMain.classList.add("main-nav--closed");
+  }
+});
+
+navButtonOpen.addEventListener("click", function () {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+navMain.classList.add("main-nav--opened");
+});
+
+var popup = document.querySelector(".popup");
+var openPopupButton = document.querySelector(".form__button");
+var popupSuccess = popup.querySelector(".popup__success");
+var popupError = popup.querySelector(".popup__error");
+var closePopupSuccessButton = popup.querySelector(".popup__btn--success");
+var closePopupErrorButton = popup.querySelector(".popup__btn");
+var inputElementNames = [];
+var inputElements = [];
+
+var isFormDirty = false;
+var isFormValid = false;
+
+function initInputElementNames(formElement) {
+
+var nameElement = formElement.querySelector("input[name="name"]");
+
+  if (nameElement) {
+    inputElementNames.push(nameElement.name);
+    inputElements.push(nameElement);
+  }
+
+  var surnameElement = formElement.querySelector("input[name=surname]");
+
+  if (surnameElement) {
+    inputElementNames.push(surnameElement.name);
+    inputElements.push(surnameElement);
+  }
+
+  var patronymicElement = formElement.querySelector("input[name=patronymic]");
+
+  if (patronymicElement) {
+    inputElementNames.push(patronymicElement.name);
+    inputElements.push(patronymicElement);
+  }
+
+  var phoneElement = formElement.querySelector("input[name=phone]");
+
+  if (phoneElement) {
+    inputElementNames.push(phoneElement.name);
+    inputElements.push(phoneElement);
+  }
+
+  var emailElement = formElement.querySelector("input[name=email]");
+
+  if (emailElement) {
+    inputElementNames.push(emailElement.name);
+    inputElements.push(emailElement);
+  }
+}
+
+function updateElemtnValidation(element) {
+  if (element.value === "") {
+    isFormValid = false;
+    element.classList.add("form__field-error");
+  }
+  else {
+    element.classList.remove("form__field-error");
+  }
+}
+
+function initEvents(formElement) {
+  formElement.addEventListener("input", function (evt) {
+    if (isFormDirty && inputElementNames.entries(evt.target.name)) {
+      updateFormValidation();
     }
   });
-  navButtonOpen.addEventListener("click", function() {
-    if (navMain.classList.contains("main-nav--closed")) {
-      navMain.classList.remove("main-nav--closed");
-      navMain.classList.add("main-nav--opened");
-    }
-  });
 
-  var popup = document.querySelector(".popup");
-  var openPopupButton = document.querySelector(".form__button--show");
-  var closePopupButton = popup.querySelector(".popup__btn");
-  var name = form.querySelector("[name=name]");
-  var surname = form.querySelector("[name=surname]");
-  var patronymic = form.querySelector("[patronymic]");
-  var phone = form.querySelector("[phone]");
-  var email = form.querySelector("[email]");
-  var isStorageSupport = true;
-  var nameStorage = "";
-  var surnameStorage = "";
-  var patronymicStorage = "";
-  var phoneStorage = "";
-  var emailStorage = "";
-
-  try {
-    nameStorage = localStorage.getItem("name");
-  } catch (err) {
-    isStorageSupport = false;
-  }
-
-  try {
-    surnameStorage = localStorage.getItem("surname");
-  } catch (err) {
-    isStorageSupport = false;
-  }
-
-  try {
-    patronymicStorage = localStorage.getItem("patronymic");
-  } catch (err) {
-    isStorageSupport = false;
-  }
-
-  try {
-    phoneStorage = localStorage.getItem("phone");
-  } catch (err) {
-    isStorageSupport = false;
-  }
-
-  try {
-    emailStorage = localStorage.getItem("email");
-  } catch (err) {
-    isStorageSupport = false;
-  }
-
-  name.addEventListener("keyup", function () {
-    this.classList.remove("form__field-error");
-  });
-
-  surname.addEventListener("keyup", function () {
-    this.classList.remove("form__field-errorr");
-  });
-
-  patronymic.addEventListener("keyup", function () {
-    this.classList.remove("form__field-error");
-  });
-
-  phone.addEventListener("keyup", function () {
-    this.classList.remove("form__field-error");
-  });
-
-  email.addEventListener("keyup", function () {
-    this.classList.remove("form__field-error");
-  });
-
-  openPopupButton.addEventListener("click", function (evt) {
+  formElement.addEventListener("submit", function (evt) {
     evt.preventDefault();
-    popup.classList.add("popup--open");
 
-    if (nameStorage) {
-      name.value = nameStorage;
-      surname.focus();
+    isFormDirty = true;
+    updateFormValidation();
+
+    if (isFormValid) {
+      popupSuccess.classList.add("popup__success--active");
     }
     else {
-      name.focus();
-    }
-
-    if (surnameStorage) {
-      surname.value = surnameStorage;
-      patronymic.focus();
-    }
-    else {
-      surname.focus();
-    }
-
-    if (patronymicStorage) {
-      patronymic.value = patronymicStorage;
-      phone.focus();
-    }
-    else {
-      patronymic.focus();
-    }
-
-    if (phoneStorage) {
-        phone.value = phoneStorage;
-        email.focus();
-    }
-    else{
-      phone.focus();
+      popupError.classList.add("popup__error--active");
     }
   });
 
-  form.addEventListener("submit", function (evt) {
+closePopupSuccessButton.addEventListener("click", function () {
+  popupSuccess.classList.remove("popup__success--active");
+});
 
-    if (name.value && surname.value &&  patronymic.value &&  phone.value &&  email.value &&isStorageSupport) {
-      localStorage.setItem("name", name.value);
-      localStorage.setItem("surname", surname.value);
-      localStorage.setItem("patronymic", patronymic.value);
-      localStorage.setItem("phone", phone.value);
-      localStorage.setItem("email", email.value);
-
-      name.classList.remove("form__field-error");
-      surname.classList.remove("form__field-error");
-      patronymic.classList.remove("form__field-error");
-      phone.classList.remove("form__field-error");
-      email.classList.remove("form__field-error");
-    }
-    else {
-      evt.preventDefault();
-
-      if(appointment.value === "") {
-        appointment.classList.add("form__field-error");
-      }
-
-      if(departure.value === "") {
-        departure.classList.add("form__field-error");
-      }
-
-      popup.classList.remove("form__form-error");
-      popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("form__form-error");
-      console.log("Нужно ввести даты заезда");
-    }
-  });
-
-  closePopupButton.addEventListener("click", function () {
-    popup.classList.remove("popup--open");
-  });
+closePopupErrorButton.addEventListener("click", function () {
+  popupError.classList.remove("popup__error--active");
+});
 
   window.addEventListener("keydown", function (evt) {
-      if (evt.keyCode === 27 && popup.classList.contains("popup--open")){
-        popup.classList.remove("popup--open");
-        popup.classList.remove("form__field-error");
+    if (evt.keyCode === 27) {
+      if (popupSuccess.classList.contains("popup__success--active")) {
+        popupSuccess.classList.remove("popup__success--active")
       }
+      if (popupError.classList.contains("popup__error--active")) {
+        popupError.classList.remove("popup__error--active")
+      }
+    }
   });
+}
+
+function updateFormValidation() {
+  isFormValid = true;
+  for (var i = 0; i < inputElements.length; i++) {
+    const inputElement = inputElements[i];
+    updateElemtnValidation(inputElement);
+  }
+}
+
+function init() {
+  var formElement = document.querySelector(".form");
+
+  if (!formElement) {
+    return;
+  }
+
+  initInputElementNames(formElement);
+  initEvents(formElement);
+}
+
+init();
